5ecc8af393f0406b1c831d26c99e6a619d2c0590
DhananjiMadhushika <dhananjimadushika72@gmail.com>
2025-01-06 20:06:53 +0530

    upload

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { PORT } from './secrets';
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("working")
+})
+
+app.listen(PORT, ()=>{
+    console.log("App work")
+})
