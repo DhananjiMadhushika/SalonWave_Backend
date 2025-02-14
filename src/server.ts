ac1000472fde6156e4b46444464bc784db4cb073
DhananjiMadhushika <dhananjimadushika72@gmail.com>
2025-02-14 13:35:36 +0530

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express from 'express'
+import { PrismaClient } from '@prisma/client';
+import express, {Express} from 'express';
+import { errorMiddleware } from './middlewares/errors';
+import rootRouter from './routes';
 import { PORT } from './secrets';
 
-const app = express();
+const app:Express = express();
+app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send("working")
+app.use('/api', rootRouter);
+
+export const prismaCilent = new PrismaClient({
+    log:['query']
 })
+
+app.use(errorMiddleware)
 
 app.listen(PORT, ()=>{
     console.log("App work")
